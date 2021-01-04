b7aa753a87f7e8ad730e7beb85a0d84bdaf7e81d
Qi Li <liqi17thu@gmail.com>
2021-01-04 17:10:08 +0800

    update

--- test.sh
+++ test.sh
@@ -5,17 +5,20 @@ echo "输入:"
 cat test/eval_case
 echo "输出:"
 python main.py test/eval.c test.ll; /usr/local/opt/llvm/bin/lli test.ll < test/eval_case;
+echo ""
 echo "回文"
 echo "输入:"
 cat test/palindrome_case
 echo "输出"
 python main.py test/palindrome.c test.ll; /usr/local/opt/llvm/bin/lli test.ll < test/palindrome_case;
+echo ""
 echo "快速排序"
 echo "输入"
 cat test/quicksort_case
 echo ""
 echo "输出"
 python main.py test/quicksort.c test.ll; /usr/local/opt/llvm/bin/lli test.ll < test/quicksort_case;
+echo ""
 echo "KMP字符串匹配"
 echo "输入"
 cat test/kmp_case
